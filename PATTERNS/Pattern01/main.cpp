c5efa573b988acfefba44c906597b8b65a8d79f8
Sushant Sutar <sushant.sutR@gmail.com>
2025-08-14 23:13:30 +0530

    Add files via upload

---
+++ PATTERNS/Pattern01/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,row,column;
+    cout<<"Enter Rows :";
+    cin>>row;
+    cout<<"Enter Columns :";
+    cin>>column;
+
+    for(i=1;i<=row;i++){
+        for(j=1;j<=column;j++){
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
