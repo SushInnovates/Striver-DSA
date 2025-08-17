58e1c62844fa65cab09ac2b928a1e17d03228ebb
Sushant Sutar <sushant.sutR@gmail.com>
2025-08-17 19:24:49 +0530

    Update main.cpp

--- PATTERNS/Pattern10/main.cpp
+++ PATTERNS/Pattern10/main.cpp
@@ -1,21 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,i,j;
-    cout<<"Enter N : ";
-    cin>>n;
+int main()
+{
+    /*
+    pattern 10 -
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+    */
+    int n, i, j;
+    cout << "Enter N : ";
+    cin >> n;
 
-    for(i=0;i<n;i++){
-        for(j=0;j<i;j++){
-            cout<<"*";
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
-    for(i=n;i>=0;i--){
-        for(j=1;j<=i;j++){
-            cout<<"*";
+    for (i = n; i >= 0; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
