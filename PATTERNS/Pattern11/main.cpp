c3ff6c449e9b0de38435cdaf46b8ecfeeaf740fa
Sushant Sutar <sushant.sutR@gmail.com>
2025-08-18 21:55:57 +0530

    Update main.cpp

--- PATTERNS/Pattern11/main.cpp
+++ PATTERNS/Pattern11/main.cpp
@@ -1,25 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int i,j,n;
-    cout<<"Enter N :";
-    cin>>n;
+int main()
+{
+    /*
+    Pattern 11 -
+1
+01
+101
+0101
+10101
+    */
+    int i, j, n;
+    cout << "Enter N :";
+    cin >> n;
 
     int start = 1;
-    for(i=0;i<n;i++){
-        if(i%2==0){
+    for (i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
             start = 1;
         }
-        else{
+        else
+        {
             start = 0;
         }
 
-        for(j=0;j<=i;j++)
+        for (j = 0; j <= i; j++)
         {
-            cout<<start;
+            cout << start;
             start = 1 - start;
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
