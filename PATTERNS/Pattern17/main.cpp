603efbfd9e47766e74c0d6750fe9a09b29e5c247
Sushant Sutar <sushant.sutR@gmail.com>
2025-08-24 20:10:48 +0530

    Add files via upload

---
+++ PATTERNS/Pattern17/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter  N: ";
+    cin>>n;
+
+    for(i=0;i<n;i++){
+        char ch='A';
+
+        //spaces
+ for(j=0;j<n-1-i;j++){
+            cout<<" ";
+        }
+
+        int breakpoint = (2*i+1)/2;
+        for(j=0;j<2*i+1;j++){
+            cout<<ch;
+            if(j<=breakpoint) ch++;
+            else ch--;
+        }
+        for(j=0;j<n-1-i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
