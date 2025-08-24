3ae7190fc8b7015a348ad0d1c263240a6ffa2849
Sushant Sutar <sushant.sutR@gmail.com>
2025-08-24 20:15:28 +0530

    Update main.cpp

--- PATTERNS/Pattern14/main.cpp
+++ PATTERNS/Pattern14/main.cpp
@@ -1,20 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-
-    int i,n;
+    /*
+    Pattern 14 -
+    A
+    A B
+    A B C
+    A B C D
+    A B C D E
+    */
+    int i, n;
     char ch;
-    cout<<"Enter N :";
-    cin>>n;
+    cout << "Enter N :";
+    cin >> n;
 
-    for(i=0;i<n;i++){
-        for(ch = 'A'; ch<='A' + i;ch++){
-            cout<<ch<<" ";
+    for (i = 0; i < n; i++)
+    {
+        for (ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-
 
     return 0;
 }
