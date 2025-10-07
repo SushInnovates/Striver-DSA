5b15ae78fa63e63426864a1385d3cd53a934c3c6
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-07 20:29:26 +0530

    Create DigitCount2.cpp

---
+++ BASIC MATHS FOR DSA/DIGIT CONCEPT/DigitCount2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<cmath>
+
+using namespace std;
+
+int Count(int n){
+    int cnt = (int)(log10(n) +1);
+    return cnt;
+}
+//Other Way to count Digits
+int main(){
+    int n;
+    cout<<"enter Number : ";
+    cin>>n;
+    
+    int digit = Count(n);
+    cout<<"Number if digits : "<<digit<<endl;
+    return 0;
+}
