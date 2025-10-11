b6514fc5492c08358687d897fd9515b58a3ac569
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-11 23:53:12 +0530

    Create Divisors2.cpp

---
+++ BASIC MATHS FOR DSA/PRINT ALL DIVISORS/Divisors2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void PrintDivisors(int n)
+{
+    vector<int> ls;
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            ls.push_back(i);
+        }
+        if ((n / i) != i)
+        {
+            ls.push_back(n / i);
+        }
+    }
+    sort(ls.begin(), ls.end());
+    for (auto it : ls)
+    {
+        cout << it << " ";
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    PrintDivisors(n);
+    return 0;
+}
